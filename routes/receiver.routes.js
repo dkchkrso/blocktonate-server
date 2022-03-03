ed7a0b336ffe5d26952f46d7df3e934cfe470f35
Christian Kroer Sørensen <dkchkrso@lego.com>
2022-03-03 10:11:33 +0100

    Receiver.routes created and tested

--- routes/receiver.routes.js
+++ routes/receiver.routes.js
@@ -3,6 +3,15 @@ const mongoose = require("mongoose");
 
 const Receiver = require("../models/Receiver.model");
 
+// GET /api/receivers -  Retrieves all of the receivers
+router.get("/receivers", (req, res, next) => {
+  Receiver.find()
+    // .populate('tasks')
+    .then((allReceivers) => res.json(allReceivers))
+    .catch((err) => res.json(err));
+});
+
+
 //  POST /api/receiver  -  Creates a new receiver
 router.post("/receiver", (req, res, next) => {
   const {
@@ -26,6 +35,58 @@ router.post("/receiver", (req, res, next) => {
   })
     .then((response) => res.json(response))
     .catch((err) => res.json(err));
+});
+
+//  GET /api/receivers/:receiverId -  Retrieves a specific receiver by id
+router.get("/receivers/:receiverId", (req, res, next) => {
+  const { receiverId } = req.params;
+
+ 
+  if (!mongoose.Types.ObjectId.isValid(receiverId)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+ 
+  // Each Project document has `tasks` array holding `_id`s of Task documents
+  // We use .populate() method to get swap the `_id`s for the actual Task documents
+  Receiver.findById(receiverId)
+    // .populate('tasks')
+    .then(receiver => res.status(200).json(receiver))
+    .catch(error => res.json(error));
+});
+ 
+ 
+// PUT  /api/receivers/:receiverId  -  Updates a specific receiver by id
+router.put("/receivers/:receiverId", (req, res, next) => {
+  const { receiverId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(receiverId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  Receiver.findByIdAndUpdate(receiverId, req.body, { new: true })
+    .then((updatedReceiver) => res.json(updatedReceiver))
+    .catch((error) => res.json(error));
+});
+ 
+ 
+// DELETE  /api/receivers/:receiverId  -  Deletes a specific receiver by id
+router.delete("/receivers/:receiverId", (req, res, next) => {
+  const { receiverId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(receiverId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  Receiver.findByIdAndRemove(receiverId)
+    .then(() =>
+      res.json({
+        message: `Receiver with ${receiverId} is removed successfully.`,
+      })
+    )
+    .catch((error) => res.json(error));
 });
 
 module.exports = router;
