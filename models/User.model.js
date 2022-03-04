56e3ef2f48fd845e1461e8c14993f975ba13e065
Christian Kroer Sørensen <dkchkrso@lego.com>
2022-03-04 16:23:36 +0100

    added cards to HomePage

--- models/User.model.js
+++ models/User.model.js
@@ -5,6 +5,7 @@ const userSchema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
+  NFTs: [{ type: Schema.Types.ObjectId, ref: "NFT" }],
 });
 
 module.exports = model("User", userSchema);
