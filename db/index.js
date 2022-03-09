b7ae025514f49b3d0df57ecc6b95b9f5c8c59808
Christian Kroer Sørensen <dkchkrso@lego.com>
2022-03-09 09:48:31 +0100

    commit with heroku

--- db/index.js
+++ db/index.js
@@ -9,13 +9,22 @@ const MONGO_URI = require("../utils/consts");
 
 
 
+// mongoose
+//   .connect(MONGO_URI)
+//   .then((x) => {
+//     console.log(
+//       `Connected to Mongo! Database name: "${x.connections[0].name}"`
+//     );
+//   })
+//   .catch((err) => {
+//     console.error("Error connecting to mongo: ", err);
+//   });
+
+// ...
+
 mongoose
-  .connect(MONGO_URI)
-  .then((x) => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
-  })
-  .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
-  });
+  .connect(process.env.MONGODB_URI)
+  .then(x => console.log(`Connected the Database: "${x.connections[0].name}"`))
+  .catch(err => console.error('Error connecting to mongo', err));
+
+// ...
