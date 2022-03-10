2c7fce0d849ab4c85e14263b7aeeecb05c98a6be
Christian Kroer Sørensen <dkchkrso@lego.com>
2022-03-10 12:51:50 +0100

    ..

--- routes/user.router.js
+++ routes/user.router.js
@@ -2,8 +2,6 @@ const router = require("express").Router();
 const mongoose = require("mongoose");
 
 
-
-
 const User = require("../models/User.model");
 
 // // GET /api/users -  Retrieves all of the users
